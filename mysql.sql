4439ddbb9db35b26676eef63ed03c108eb2dbdac
kyralaw <luozishan97@gmail.com>
2022-05-26 17:43:05 +0800

    Initial commit

---
+++ mysql.sql
@@ -0,0 +1,22 @@
+create database if not exists Project;
+
+use Project;
+
+create table Admin(
+                      admin_id varchar(20),
+                      admin_name varchar(25),
+                      admin_PIN varchar(30),
+                      admin_gender varchar(20)
+);
+
+INSERT into Admin value ('e1237h','Jason','123456789','Male');
+
+create table User(
+                     First_name varchar(25),
+                     Last_nmae varchar(25),
+                     User_id varchar(20),
+                     User_name varchar(25),
+                     User_PIN varchar(30)
+);
+INSERT into User value ('Kyra','Luo','K12345','KYRA','123456789');
+
